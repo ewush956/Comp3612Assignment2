abd2fa9e6b0b363c146f0ab011ee08a23094cf60
Kyle Scidmore <kscid125@mtroyal.ca>
2024-11-15 09:15:42 -0700

    ignore json file

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function process_race_data(current_season){
             console.log(data);
             populate_race_data(data, current_season);
 
-            race_click(data);
+            race_click(current_season);
 
         })
         .catch(error =>{
@@ -77,7 +77,7 @@ function process_race_data(current_season){
     }
 }
 
-function race_click(race_data){
+function race_click(current_season){
 
     let race_click = document.querySelector("#race_table");
 
@@ -94,18 +94,35 @@ function race_click(race_data){
 
             fetch(`http://www.randyconnolly.com/funwebdev/3rd/api/f1/qualifying.php?race=${race_id}`)
             .then(response => response.json())
-            .then(data =>{
+            .then(qual_data =>{
     
     
-                console.log(data);
+                console.log(qual_data);
 
-                populate_qaul_data(data);
+                populate_qaul_data(qual_data);
+
+                fetch(`http://www.randyconnolly.com/funwebdev/3rd/api/f1/results.php?race=${race_id}`)
+                .then(response => response.json())
+                .then(results_data =>{
+        
+        
+                    console.log(results_data);
+    
+                    populate_results_data(results_data);
+    
+        
+                })
+                .catch(error =>{
+        
+                });
 
     
             })
             .catch(error =>{
     
             });
+
+            
             
         }
 
@@ -119,7 +136,6 @@ function populate_race_data(data, current_season){
 
     const race_list = document.querySelector("#race_table");
     race_list.innerHTML = "";
-    let round_temp = 1;
 
     let race_header = document.querySelector("#race_header");
 
@@ -133,8 +149,7 @@ function populate_race_data(data, current_season){
 
         let round = document.createElement("td");
         round.classList.add("px-4", "py-2", "border-b", "text-sm", "text-gray-800");
-        round.textContent = `${round_temp}`;
-        round_temp++;
+        round.textContent = `${race.round}`;
 
         let name = document.createElement("td");
         name.classList.add("px-4", "py-2", "border-b", "text-sm", "text-gray-800");
@@ -149,7 +164,7 @@ function populate_race_data(data, current_season){
         
     }
 
-};
+}
 
 function populate_qaul_data(data){
 
@@ -189,9 +204,6 @@ function populate_qaul_data(data){
         q3.classList.add("px-4", "py-2", "border-b", "text-sm", "text-gray-800");
         q3.textContent = `${d.q3}`;
         
-
-
-
         row.appendChild(pos);
         row.appendChild(name);
         row.appendChild(constructor);
@@ -199,27 +211,98 @@ function populate_qaul_data(data){
         row.appendChild(q2);
         row.appendChild(q3);
 
-
-
         qual_table.appendChild(row);
+
+    }
+}
+
+function populate_results_data(data){
+
+    const results_table = document.querySelector("#results_table");
+    const first = document.querySelector("#first");
+    const second = document.querySelector("#second");
+    const third = document.querySelector("#third");
+    const first_box = document.querySelector("#first_box");
+    const second_box = document.querySelector("#second_box");
+    const third_box = document.querySelector("#third_box");
+
+    results_table.innerHTML = "";
+
+    first.textContent = `${data[0].driver.forename} ${data[0].driver.surname}`;
+    second.textContent = `${data[1].driver.forename} ${data[1].driver.surname}`;
+    third.textContent = `${data[2].driver.forename} ${data[2].driver.surname}`;
+
+    first_box.classList.add("hover:bg-gray-200", "cursor-pointer");
+    second_box.classList.add("hover:bg-gray-200", "cursor-pointer");
+    third_box.classList.add("hover:bg-gray-200", "cursor-pointer");
+
+    for(let d of data){
+
+        let row = document.createElement("tr");
+
+        let pos = document.createElement("td");
+        pos.classList.add("px-4", "py-2", "border-b", "text-sm", "text-gray-800");
+        pos.textContent = `${d.position}`;
+
+        let name = document.createElement("td");
+        name.classList.add("px-4", "py-2", "border-b", "text-sm", "text-gray-800", "hover:bg-gray-200", "cursor-pointer");
+        name.textContent = `${d.driver.forename} ${d.driver.surname}`;
+
+        let constructor = document.createElement("td");
+        constructor.classList.add("px-4", "py-2", "border-b", "text-sm", "text-gray-800", "hover:bg-gray-200", "cursor-pointer");
+        constructor.textContent = `${d.constructor.name}`;
+
+        let laps = document.createElement("td");
+        laps.classList.add("px-4", "py-2", "border-b", "text-sm", "text-gray-800");
+        laps.textContent = `${d.laps}`;
+
+        let points = document.createElement("td");
+        points.classList.add("px-4", "py-2", "border-b", "text-sm", "text-gray-800");
+        points.textContent = `${d.points}`;
+
+        row.appendChild(pos);
+        row.appendChild(name);
+        row.appendChild(constructor);
+        row.appendChild(laps);
+        row.appendChild(points);
+
+        results_table.appendChild(row);
 
     }
 }
 
 function change_view(current_view){
 
-    let home_view = document.querySelector("#home");
-    let race_view = document.querySelector("#race");
-    let buttons = document.querySelector("#nav_buttons");
-    let qual_table = document.querySelector("#qual_table");
+    const home_view = document.querySelector("#home");
+    const race_view = document.querySelector("#race");
+    const buttons = document.querySelector("#nav_buttons");
+    const title = document.querySelector("#title")
+    const qual_table = document.querySelector("#qual_table");
+    const results_table = document.querySelector("#results_table");
+    const first = document.querySelector("#first");
+    const second = document.querySelector("#second");
+    const third = document.querySelector("#third");
+    const first_box = document.querySelector("#first_box");
+    const second_box = document.querySelector("#second_box");
+    const third_box = document.querySelector("#third_box");
+
+    first.textContent = "";
+    second.textContent = "";
+    third.textContent = "";
 
     qual_table.innerHTML = "";
+    results_table.innerHTML = "";
+
+    first_box.classList.remove("hover:bg-gray-200", "cursor-pointer");
+    second_box.classList.remove("hover:bg-gray-200", "cursor-pointer");
+    third_box.classList.remove("hover:bg-gray-200", "cursor-pointer");
 
     if(current_view === "home"){
 
         home_view.classList.add("hidden");
+        title.classList.add("hidden");
         race_view.classList.remove("hidden");
-        buttons.classList.remove("hidden");
+        buttons.classList.remove("hidden", "lg:hidden");
 
         current_view = "race";
 
@@ -228,8 +311,9 @@ function change_view(current_view){
     }else{
 
         home_view.classList.remove("hidden");
+        title.classList.remove("hidden");
         race_view.classList.add("hidden");
-        buttons.classList.add("hidden");
+        buttons.classList.add("hidden", "lg:hidden");
 
         current_view = "home";
 
@@ -239,4 +323,4 @@ function change_view(current_view){
 
 
 
-};
+}
